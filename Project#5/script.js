fa18eb8a29b01ecf4682106499a240e3db9b48bc
Collins Kiprono <one.kcollin@gmail.com>
2022-02-23 21:21:54 +0300

    New practice files

--- Project#5/script.js
+++ Project#5/script.js
@@ -74,4 +74,141 @@ console.log(pizza, risotto, otherFood);
 const { sat, ...weekdays } = restaurant.openingHours;
 console.log(weekdays);
 
+const menu1 = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(team, odd);
+  console.log(`${teamStr} ${odd}`);
+}
+
+// const orderSet = new Set([
+// 'Pasta',
+// 'Pizza',
+// 'Rice'
+// ]);
+// console.log(orderSet);
+// console.log(orderSet.size);
+// console.log(orderSet.has('Pizza'));
+
+// const staff = ['Waiter', 'Chef', 'Manager'];
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(staffUnique.length);
+
+// const rest = new Map();
+// rest.set('name', 'CJ');
+// rest.set(1, 'Italy');
+// rest.set(2, 'Kenya');
+// console.log(rest);
+
+// rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']);
+
+// console.log(rest.has('categories'));
+
+const question = new Map([
+  ['question', 'What is the best programming language?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Corect'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+const gameEvents = new Map([
+  [17, 'Goal'],
+  [36, 'Subsitution'],
+  [44, 'Yello Card'],
+  [45, 'Hal Time'],
+]);
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+
+for (const [min, event] of gameEvents) {
+  console.log(`${min}: ${event}`);
+}
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+const text = document.querySelector('textarea').value;
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  
+  for (const row of rows) {
+    const [first, last] = row.toLocaleUpperCase().trim().split('_');
+    console.log(row);
+  }
+});
+
+const flight =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const getCode = str => str.toUpperCase().slice(0,3);
+
+for (const f of flight.split('+')) {
+ const [type, from, to, time] =  f.split(';');
+ const output = `${type.replaceAll('_', ' ')} ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`;
+ console.log(output);
+}
